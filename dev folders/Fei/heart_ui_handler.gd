f9e288d6878864fae3de01bcb6350e1a10b7cbfd
Blixt <blixtdraken@gmail.com>
2025-01-08 16:36:13 +0100

    Started Heart ui handling

---
+++ dev folders/Fei/heart_ui_handler.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+var heart_sprites:Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	heart_sprites = get_children()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
