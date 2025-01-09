309a23d37538d138956850dd0d7511ded5d75b41
baptul-bim <baptul.bim@gmail.com>
2025-01-09 15:27:06 +0100

    world_tile

---
+++ dev folders/Louie/world_tile.gd
@@ -0,0 +1,20 @@
+class_name WorldTiles
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func tile_check(tile_pos:Vector2i)->bool:
+	
+	
+	
+	return true
+	
