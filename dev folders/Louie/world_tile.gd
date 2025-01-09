889d04f163f550734c3134a9228696226bf0abf5
baptul-bim <baptul.bim@gmail.com>
2025-01-09 16:05:52 +0100

    epic cartride into unhappy marriage

--- dev folders/Louie/world_tile.gd
+++ dev folders/Louie/world_tile.gd
@@ -2,6 +2,8 @@ class_name WorldTiles
 extends TileMapLayer
 
 
+enum TileCheckData {FLOOR, BREAKABLE, BORDER, ENEMY}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,9 +14,14 @@ func _process(delta):
 	pass
 
 
-func tile_check(tile_pos:Vector2i)->bool:
+func tile_check(tile_pos:Vector2i)->TileCheckData:
 	
+	if get_cell_source_id(tile_pos) == 2:
+
+		erase_cell(tile_pos)
+		return TileCheckData.BREAKABLE
+		
+
+	return TileCheckData.FLOOR
 	
-	
-	return true
 	
