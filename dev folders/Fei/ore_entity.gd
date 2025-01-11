fc054f5c852aa238a1160e5184faf38d63fc6b5c
Blixt <blixtdraken@gmail.com>
2025-01-11 19:18:38 +0100

    dsadas

--- dev folders/Fei/ore_entity.gd
+++ dev folders/Fei/ore_entity.gd
@@ -4,7 +4,8 @@ class_name OreEntity
 
 @export var ore_value : int
 
-
+@export
+var has_hidden_hole:bool = false
 
 func _on_damage(damage:int):
 	
@@ -12,12 +13,13 @@ func _on_damage(damage:int):
 	print(GlobalScore.collected_ore) #TEST
 	world_tiles.tile_entities.erase(tile_pos)
 	
-	var hole_scene:PackedScene = preload("res://scenes/instantiable/hole.tscn")
-	
-	var hole:HoleEntity = hole_scene.instantiate()
-	hole.world_tiles = world_tiles
-	hole.spawn_tile = tile_pos
-	get_parent().add_child(hole)
+	if has_hidden_hole:
+		var hole_scene:PackedScene = preload("res://scenes/instantiable/hole.tscn")
+		
+		var hole:HoleEntity = hole_scene.instantiate()
+		hole.world_tiles = world_tiles
+		hole.spawn_tile = tile_pos
+		get_parent().add_child(hole)
 	
 	queue_free()
 	pass
