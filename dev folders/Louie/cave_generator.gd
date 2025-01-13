f3d2f160a93c99dd27eba21a22400ebf1cfd7c68
baptul-bim <baptul.bim@gmail.com>
2025-01-13 02:34:01 +0100

    gonatt

--- dev folders/Louie/cave_generator.gd
+++ dev folders/Louie/cave_generator.gd
@@ -1,81 +1,50 @@
 extends Node2D
 
-@export var dirt_tilemap : TileMapLayer
-@export var wall_tilemap : TileMapLayer
+#const Player = preload("res://Player.tscn")
+#const Exit = preload("res://ExitDoor.tscn")
 
-var rng = RandomNumberGenerator.new()
+var borders = Rect2(1, 1, 38, 21)
 
-var CellSize = Vector2(16, 16)
-var width = 1024/CellSize.x
-var height = 1024/CellSize.y
-var grid = []
-
-var Tiles = {
-	"empty": -1,
-	"wall": 0,
-	"floor": 1
-}
-
-func _init_grid():
-	grid = []
-	for x in width:
-		grid.append([])
-		for y in height:
-			grid[x].append(-1);
-
-func GetRandomDirection():
-	var directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-	var direction = directions[rng.randi()%4]
-	return Vector2(direction[0], direction[1])
-	
-func _create_random_path():
-	var max_iterations = 1000
-	var itr = 0
-	
-	var walker = Vector2.ZERO
-	
-	while itr < max_iterations:
-		
-		# Perform random walk
-		# 1- choose random direction
-		# 2- check that direction is in bounds
-		# 3- move in that direction
-		var random_direction = GetRandomDirection()
-		
-		if (walker.x + random_direction.x >= 0 and 
-			walker.x + random_direction.x < width and
-			walker.y + random_direction.y >= 0 and
-			walker.y + random_direction.y < height):
-				
-				walker += random_direction
-				grid[walker.x][walker.y] = Tiles.floor
-				itr += 1
-	
-func _spawn_tiles():
-	
-	for x in width:
-		for y in height:
-  
-			match grid[x][y]:
-				Tiles.empty:
-					pass
-				Tiles.floor:
-					dirt_tilemap.set_cellv(Vector2(x, y), 0)
-				Tiles.wall:
-					pass
-
-func _clear_tilemaps():
-	for x in width:
-		for y in height:
-			dirt_tilemap.clear()
-			wall_tilemap.clear()
-  
-	dirt_tilemap.update_bitmask_region()
-	wall_tilemap.update_bitmask_region()
+@onready var tileMap = get_parent().get_node("WorldTiles")
 
 func _ready():
-	rng.randomize()
-	_init_grid()
-	_clear_tilemaps()
-	_create_random_path()
-	_spawn_tiles()
+	randomize()
+	generate_level()
+
+
+func generate_level():
+
+	var walker = Walker.new(Vector2(19, 11), borders)
+
+	var map = walker.walk(500)
+
+	#var player = Player.instantiate()
+	#add_child(player)
+	#player.position = map.front()*32
+	
+	#var exit = Exit.instantiate()
+	#add_child(exit)
+	#exit.position = walker.get_end_room().position*32
+	#exit.connect("leaving_level", Callable(self, "reload_level"))
+
+	walker.queue_free()
+
+	
+
+	for location in map:
+
+		var walked = []
+
+		walked.append(location)
+
+		tileMap.set_cells_terrain_connect(walked, 0, 0)
+		#print(tileMap)
+
+		#await get_tree().create_timer(0.001).timeout
+
+func reload_level():
+	get_tree().reload_current_scene()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		reload_level()
