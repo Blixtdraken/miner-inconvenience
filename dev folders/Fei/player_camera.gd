b95c079d43930452dc2641943d5754bd815e3828
Blixt <blixtdraken@gmail.com>
2025-01-15 18:08:54 +0100

    closugn

--- dev folders/Fei/player_camera.gd
+++ dev folders/Fei/player_camera.gd
@@ -9,7 +9,7 @@ var follow_speed:float = 10
 
 var functional_pos:Vector2 = Vector2.ZERO
 func _process(delta):
-	var zoom_level:float = get_viewport().size.x/pixel_limit
+	var zoom_level:float = get_viewport().size.x/GlobalSettings.pixel_limit
 	zoom = Vector2(zoom_level, zoom_level)
 	
 	functional_pos = functional_pos.lerp(get_parent().global_position, delta*follow_speed)
