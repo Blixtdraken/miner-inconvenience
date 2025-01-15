a8f540ede34fad7a7c67154cef09fb64e79774a1
baptul-bim <baptul.bim@gmail.com>
2025-01-15 08:35:33 +0100

    lite changes

--- dev folders/Louie/hud_manager.gd
+++ dev folders/Louie/hud_manager.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 
 @onready var gold_text = $Control/MarginContainer/GoldText
+@onready var health_text = $Control/MarginContainer/HealthText
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	gold_text.text = str(GlobalScore.collected_ore)
-	
+	health_text.text = str(GlobalHealth.player_hp)
 	pass
 
 
